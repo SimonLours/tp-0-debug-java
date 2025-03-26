9180d8230fd9fd6cad1b3a650f084e34235810ff
SimonLours <lourss@3il.fr>
2025-03-26 14:03:07 +0100

    td0

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Problème 1
         int[] numbers = {1, 2, 3, 4, 5};
-        for (int i = 0; i <= numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println("Number at index " + i + ": " + numbers[i]);
         }
 
@@ -14,27 +14,34 @@ public class Main {
         int count = 10;
         while (count > 0) {
             System.out.println("Count: " + count);
-            count += 2;
+            count--;
         }
 
         // Problème 4
         String message = "Bonjour";
         System.out.println("Longueur du message : " + message.length());
-        char character = message.charAt(10);
-        System.out.println("Caractère à l'indice 10 : " + character);
+        int index = 10;
+        if (index < message.length()) {
+            char character = message.charAt(index);
+            System.out.println("Caractère à l'indice 10 : " + character);
+        }
 
         // Problème 5
         String str1 = "Hello";
         String str2 = new String("Hello");
-        System.out.println("Comparaison de chaînes : " + (str1 == str2));
+        System.out.println("Comparaison de chaînes : " + str1.equals(str2));
 
         // Problème 6
-        Main mainObj = null;
+        Main mainObj = new Main();
         mainObj.method();
     }
 
     // Méthode pour le problème de division par zéro
     public static int calculateValue(int a, int b) {
+        if(b == 0) {
+            System.out.println("Erreur: division par zéro");
+            return 0; // ou lever une exception selon le besoin
+        }
         return a / b;
     }
 
